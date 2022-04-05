c196c547b04c2ea5382ff97f67c1bdf183ccbec4
luftedar <ugurorcun91@gmail.com>
2022-04-05 15:54:26 +0300

    Created Basic Component Structure

---
+++ src/components/cruises/CruiseDetail.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CruiseDetail() {
+  return (
+    <div>CruiseDetail</div>
+  )
+}
+
+export default CruiseDetail
---
+++ src/components/cruises/CruiseMain.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CruiseMain() {
+  return (
+    <div>CruiseMain</div>
+  )
+}
+
+export default CruiseMain
