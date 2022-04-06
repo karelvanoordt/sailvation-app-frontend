ead752ea756ec7f4b8c6f72ce9c87016164b57f2
luftedar <ugurorcun91@gmail.com>
2022-04-06 13:45:08 +0300

    Add Action Callers for Reservations & Update Reservation Reducer

--- src/redux/Reservations/reservations.js
+++ src/redux/Reservations/reservations.js
@@ -1,9 +1,43 @@
 import * as apiCalls from '../../services/services';
 
+const FETCH_ALL_RESERVATIONS = 'FETCH_ALL_RESERVATIONS';
+const POST_NEW_RESERVATION = 'POST_NEW_RESERVATION';
+const DELETE_RESERVATION = 'DELETE_RESERVATION';
+
 const initialReservationState = [];
+
+export const fetchAllReservations = (userId) => async (dispatch) => {
+  const payload = await apiCalls.fetchAllReservations(userId);
+  dispatch({
+    type: FETCH_ALL_RESERVATIONS,
+    payload
+  });
+};
+
+export const postNewReservations = (newReservationData) => async (dispatch) => {
+  const payload = await apiCalls.postNewCruise(newReservationData);
+  dispatch({
+    type: POST_NEW_RESERVATION,
+    payload
+  });
+};
+
+export const deleteReservation = (userId, reservationId) => async (dispatch) => {
+  await apiCalls.deleteReservation(userId, reservationId);
+  dispatch({
+    type: DELETE_RESERVATION,
+    reservationId
+  })
+}
 
 const reservationReducer = (state = initialReservationState, action) => {
   switch (key) {
+    case FETCH_ALL_RESERVATIONS:
+      return action.payload;
+    case POST_NEW_RESERVATION:
+      return [...state, ...action.payload];
+    case DELETE_RESERVATION:
+      return state.filter((reservationElement) => reservationElement.reservationId !== action.reservationId);
     default:
       state;
   }
