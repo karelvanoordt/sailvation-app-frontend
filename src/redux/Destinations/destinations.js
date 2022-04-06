eba0088d61f2f6784ff48bf0712e64783ed6d337
luftedar <ugurorcun91@gmail.com>
2022-04-06 13:37:04 +0300

    Add Action Callers for Destinations & Update Destination Reducer

--- src/redux/Destinations/destinations.js
+++ src/redux/Destinations/destinations.js
@@ -1,12 +1,24 @@
 import * as apiCalls from '../../services/services';
 
+const FETCH_ALL_DESTINATIONS = 'FETCH_ALL_DESTINATIONS';
+
 const initialDestinationState = [];
+
+export const fetchAllDestinations = (userId) => async (dispatch) => {
+  const payload = await apiCalls.fetchAllDestinations(userId);
+  dispatch({
+    type: FETCH_ALL_DESTINATIONS,
+    payload
+  });
+};
 
 const destinationReducer = (state = initialDestinationState, action) => {
   switch (action.type) {
+    case FETCH_ALL_DESTINATIONS:
+      return action.payload;
     default:
       return state;
-  }
+  };
 };
 
 export default destinationReducer;
