c1f55a5f70a46b470064d7e1945fb1f9bca2efc7
hamid-murambiwa <machipisajunior@gmail.com>
2022-04-07 15:36:43 +0200

    Remove airbnb

--- src/components/reservations/ReservationMain.js
+++ src/components/reservations/ReservationMain.js
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import fetchAllReservations from '../../services/services';
 import { Link } from 'react-router-dom';
 
 function ReservationMain() {
+  const reservations = useSelector((state) => state.reservationReducer);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchAllReservations(1));
+  }, [dispatch]);
+
   return (
     <div>
-      <h1>ReservationMain</h1>
-      <ul>
-        <li>
-          <Link to="/cruises/:id/reservation/:id/destination">
-            Destinations(ReservationDetails)
-          </Link>
-        </li>
+      <ul className="cruise-list">
+    {reservations.map((reservation) => (
+      <li key={reservation.id} className="reservation-list-item">
+      <div>
+        <h2>{reservation.start_date}</h2>
+        <p>{reservation.finish_dat}</p>
+      </div>
+      <Link to={`/cruises/${reservation.cruise_id}/reservation/${reservation.id}/destination`}>
+        Reservation
+      </Link>
+    </li>
+    ))}
       </ul>
     </div>
   );
