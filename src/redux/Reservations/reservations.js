cb11bfc0d89d1a1b83fd6b8fe6d22750bf317205
Busiwa24 <busiwaliuma@gmail.com>
2022-04-11 01:43:43 +0200

    Add input to fetch all reservations

--- src/redux/Reservations/reservations.js
+++ src/redux/Reservations/reservations.js
@@ -5,3 +5,17 @@ const DELETE_RESERVATION = 'reservation/deleteReservation';
 const END_POINT = 'http://localhost:3000';
 const API_ROUTE = '/api/v1/';
 
+export const createReservationAction = (payload) => ({
+  type: CREATE_RESERVATION,
+  payload,
+});
+
+export const getAllReservationAction = (payload) => ({
+  type: FETCH_ALL_RESERVATION,
+  payload,
+});
+
+export const deleteAReservationAction = (payload) => ({
+  type: DELETE_RESERVATION,
+  payload,
+});
