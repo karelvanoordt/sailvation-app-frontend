1cb60e441a370f54ecaabf6b502f530041929ef7
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-12 05:28:14 -0500

    Update users reducer

--- src/components/login/FormSignup.js
+++ src/components/login/FormSignup.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { postNewUser } from '../../services/services';
+import { fetchAllUsers, postNewUser } from '../../services/services';
 
 function SignupForm({ Signup }) {
   const [details, setDetails] = useState({ name: '', email: '' });
---
+++ src/redux/Users/users.js
@@ -0,0 +1,35 @@
+import * as apiCalls from '../../services/services';
+
+const FETCH_ALL_USERS = 'FETCH_ALL_USERS';
+const POST_NEW_USER = 'POST_NEW_USER';
+
+const initialUserState = [];
+
+export const fetchAllUsers = () => async (dispatch) => {
+  const payload = await apiCalls.fetchAllUsers();
+  dispatch({
+    type: FETCH_ALL_USERS,
+    payload,
+  });
+};
+
+export const postNewUser = (newUserData) => async (dispatch) => {
+  const payload = await apiCalls.postNewUser(newUserData);
+  dispatch({
+    type: POST_NEW_USER,
+    payload,
+  });
+};
+
+const userReducer = (state = initialUserState, action) => {
+  switch (action.type) {
+    case FETCH_ALL_USERS:
+      return action.payload;
+    case POST_NEW_USER:
+      return [...state, ...action.payload];
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
