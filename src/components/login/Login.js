b31da46b94da68a5710b6b2e8ed9d0525159a1cf
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-12 08:29:05 -0500

    Update login

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,37 +1,50 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router' ;
 import Cruise from '../cruises/Cruise';
+import { fetchAllUsers } from '../../redux/Users/users';
 
 export default function LoginPage() {
-  const adminUser = {
-    email: 'admin@admin.com',
-    name: 'Karel',
-  };
-
   const dispatch = useDispatch();
-  const [userEmail, setEmail] = useState('');
+  const [userEmail, setUserEmail] = useState('');
+  // const Login = (details) => {
+  //   console.log(details);
+  //   if (details.email === adminUser.email && details.name === adminUser.name) {
+  //   //   console.log('Logged in');
+  //     setUser({
+  //       name: details.name,
+  //       email: details.email,
+  //     });
+  //   } else {
+  //   //   console.log('Details not match');
+  //   }
+  // };
 
+  const navigate = useNavigate();
+  // const userId = 1;
 
-  const Login = (details) => {
-    console.log(details);
-    if (details.email === adminUser.email && details.name === adminUser.name) {
-    //   console.log('Logged in');
-      setUser({
-        name: details.name,
-        email: details.email,
-      });
-    } else {
-    //   console.log('Details not match');
-    }
+  useEffect(() => {
+    dispatch(fetchAllUsers());
+  }, []);
+
+  const users = useSelector((state) => state.userReducer);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+    users.map((user) => {
+      if (user.email === userEmail) {
+        navigate(`/${user.id}`);
+      }
+      return 'Please sign up';
+    });
   };
-
 
   return (
 
     <div className="login-container">
 
-      {(user.email !== '') ? (
+      {(userEmail !== '') ? (
         <Cruise />
       ) : (
         <div>
@@ -40,17 +53,19 @@ export default function LoginPage() {
               <div className="form-inner">
                 <h2>Login</h2>
                 {/* ERROR! */}
-                <div className="form-group">
+                {/* <div className="form-group">
                   <label htmlFor="name">
                     Name:
-                    <input type="text" name="name" id="name" onChange={(e) => setDetails({ ...details, name: e.target.value })} value={details.name} />
+                    <input type="text" name="name" id="name" onChange={(e) =>
+                    setDetails({ ...details, name: e.target.value })}
+                    value={details.name} />
                   </label>
-                </div>
+                </div> */}
 
                 <div className="form-group">
                   <label htmlFor="email">
                     Email:
-                    <input type="email" name="email" id="email" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} />
+                    <input type="email" name="email" id="email" onChange={(e) => setUserEmail(e.target.value)} value={userEmail} />
                   </label>
                 </div>
 
