2e1d687a98a9e8f491506f489ea0bb0ff9815cf9
Karel van Oordt <vanoordtkarel@gmail.com>
2022-04-13 04:46:01 -0500

    Update signup form

--- src/components/login/SignupForm.js
+++ src/components/login/SignupForm.js
@@ -1,47 +1,62 @@
-/* eslint-disable*/
-
-import React, { useReducer, useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { Link } from 'react-router-dom';
 import { postNewUser } from '../../redux/Users/users';
- 
 
 function SignupForm() {
-
   const dispatch = useDispatch();
   const [details, setDetails] = useState(
-    {name: '', email: ''}
+    { name: '', email: '' },
   );
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const submitHandler = (e) => {
     e.preventDefault();
-    if ( details.name !== '' && details.email !== '' ) {
+    if (details.name !== '' && details.email !== '') {
       dispatch(postNewUser(details));
+      setIsLoggedIn(true);
     }
-  }
+  };
 
   return (
-    <form onSubmit={submitHandler}>
-      <div className="form-inner">
-        <h2>Signup</h2>
-        {/* ERROR! */}
-        <div className="form-group">
-          <label htmlFor="name">
-            Name:
-            <input type="text" name="name" id="name" onChange={(e) => setDetails({ ...details, name: e.target.value })} value={details.name} />
-          </label>
+    <div>
+      {isLoggedIn
+        && (
+        <div>
+          <p>Registarion was successfull</p>
+          <Link to="/login">
+            You can login from here
+          </Link>
         </div>
+        )}
+      <form
+        onSubmit={() => {
+          submitHandler();
+          isLoggedIn(true);
+        }}
+      >
+        <div className="form-inner">
+          <h2>Signup</h2>
+          {/* ERROR! */}
+          <div className="form-group">
+            <label htmlFor="name">
+              Name:
+              <input type="text" name="name" id="name" onChange={(e) => setDetails({ ...details, name: e.target.value })} value={details.name} />
+            </label>
+          </div>
 
-        <div className="form-group">
-          <label htmlFor="email">
-            Email:
-            <input type="email" name="email" id="email" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} />
-          </label>
+          <div className="form-group">
+            <label htmlFor="email">
+              Email:
+              <input type="email" name="email" id="email" onChange={(e) => setDetails({ ...details, email: e.target.value })} value={details.email} />
+            </label>
+          </div>
+
+          <input type="submit" value="Signup" />
         </div>
-
-        <input type="submit" value="Signup" />
-      </div>
-    </form>
+      </form>
+    </div>
   );
 }
 
